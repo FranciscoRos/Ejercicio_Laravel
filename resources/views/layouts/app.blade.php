41ebf4e7366ac9dcf587e4c5a3b3d1583afdaf25
FranciscoRos <L21390377@chetumal.tecnm.mx>
2025-11-04 20:25:14 -0500

    Subiendoo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,65 @@
-<html>
-  <head>
-    <title>@yield('title', 'Mi App')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-  </head>
-  <body>
-    
-    <header>
-      @yield(section: 'header')
-      <title style="text-align: center; color: red;"  >@yield('title', 'Mi App')</title>
-      <h1 style="text-align: center; color: red;"  >@yield('tituloReal', 'Mi App')</h1>
-    </header>
+<!doctype html>
+<html lang="en">
 
-    <main>
-      @yield('content')
-    </main>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    crossorigin="anonymous" />
+  <title>@yield('title', 'Online Store')</title>
+</head>
 
-    <aside>
-      @yield('sidebar')
-      <div class="overflow-x-auto rounded-lg border border-gray-200 shadow-md m-5">
-  <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
-    <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-      <tr class="hover:bg-gray-50">
-        <td class="px-6 py-4"> @stack('tablita')</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+<body>
+  <!-- header -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
+    <div class="container">
+      <a class="navbar-brand" href="#">Online Store</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link active" href="#">About</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <header class="masthead bg-primary text-white text-center py-4">
+    <div class="container d-flex align-items-center flex-column">
+      <h2>@yield('subtitle', 'A Laravel Online Store')</h2>
+    </div>
+  </header>
+  <!-- header -->
+  <div class="container my-4">
+    @yield('content')
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+  </script>
+</body>
 
-    </aside>
+</html>
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+<title>@yield('title', 'Online Store')</title>
+</head>
 
-    <footer>
-      @yield('footer' )
-        <x-footer>
-          Este es el contenido que va dentro del footer en la plantilla.
-        </x-footer>
-        @show
-    </footer>
-  </body>
+<body>
+  …
+  <!-- footer -->
+  <div class="copyright py-4 text-center text-white">
+    <div class="container">
+      <small>
+        Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+          href="https://twitter.com/danielgarax">
+          Daniel Correa
+        </a> - <b>Paola Vallejo</b>
+      </small>
+    </div>
+  </div>
+  <!-- footer -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+  </script>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\postController;
-use App\Http\Controllers\postsController;
-Route::get('/', [HomeController::class, 'index']);
+use App\Http\Controllers\HomeController; // <--- 1. IMPORTA EL CONTROLADOR AQUÍ
 
-
- $variable = "chupa peness";
-
-Route::get('/penesito', function () {
-    return view('penesito', ['varia' => $variable = "chupa penes" ]);
+Route::get('/', function () {
+    $viewData = [];
+    $viewData["title"] = "Home Page - Online Store";
+    return view('home.index')->with("viewData", $viewData);
 });
 
-Route::fallback(function () {
-    return redirect('/');
-});
-
-//Esta es una ruta con parámetros
-//Route::get('/posts/{numeroPost}', function ($IdPost){
-  //  return "Este es un post dinámicocon su id: {$numeroPost}";
-//});
-
-Route::get('/posts/{id}', 
-[PostsController::class, 'postsId' ] );
-
-
-
-Route::get('/posts', PostsController::class);
-
-//Esta es una ruta con parámetros dobles
-Route::get('/posts/{IdPost}/comments/{IdComment}', function ($IdPost, $IdComment){
-    return "Este es un comentario {$IdComment} del post {$IdPost}";
-});
-//Ejemplo de ruta con parámetros dobles
-//Servidor/posts/45/comments/67
-
-//Esta es una ruta con parámetros opcionales
-Route::get('/usuarios/{name?}', function ($name = "Invitados"){
-    return "Bienvenido {$name}";
-});
-
-//Esta es una ruta con parámetros opcionales que se comporta diferente si no se envía el parámetro
-Route::get('/usuario/{name?}', function ($name = null){
-    if (!$name) {
-        return "Bienvenido Invitado";
-    }
-    return "Bienvenido {$name}";
-});
+// 2. USA LA SINTAXIS DE ARRAY (CLASE::CLASS, 'METODO')
+Route::get('/about', [HomeController::class, 'about'])->name("home.about");
