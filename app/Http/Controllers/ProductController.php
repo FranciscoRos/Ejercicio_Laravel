e5ec4b81fe2578d1a2f3c5ebeea1b5bab5c4883a
FranciscoRos <l21390377@chetumal.tecnm.mx>
2025-11-05 19:31:08 -0500

    subiendo pag 53

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,12 +5,6 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller 
 {
-    public static $products = [ 
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "image" => "game.png", "price"=>"1000"], 
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "image" => "safe.png", "price"=>"999"], 
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "image" => "submarine.png", "price"=>"30"], 
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "image" => "game.png", "price"=>"100"] 
-    ];
 
     public function index() 
     {
@@ -25,8 +19,8 @@ class ProductController extends Controller
     {
         $viewData = []; 
         $product = Product::findOrFail($id);
-        $viewData["title"] = $product["name"]." - Online Store"; 
-        $viewData["subtitle"] = $product["name"]." - Product information"; 
+        $viewData["title"] = $product->getName()." - Online Store"; 
+        $viewData["subtitle"] = $product->getName()." - Product information"; 
         $viewData["product"] = $product; 
         return view('product.show')->with("viewData", $viewData); 
     }
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::get('/about', [HomeController::class, 'about'])->name("home.about");
 // Rutas para listas productos
 Route::get('/products', [ProductController::class, 'index']) -> name("product.index");
 Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.show");
+
+// Rutas para el área de administración
+Route::get('/admin', [App\Http\Controllers\Admin\AdminHomeController::class, 'index'])->name("admin.home.index");
